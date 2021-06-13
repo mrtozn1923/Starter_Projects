efdca74e6e2fea1c9e54a234b001e7b725800168
mrtozn1923 <mrtozn1923@gmail.com>
2021-06-13 19:13:27 +0300

    HTML-SASS-VanillaJS-Bootstrap5

--- Webpack5_Full_Project_Setup-HTML-SASS-Vanilla_JS-Bootstrap5/src/index.js
+++ Webpack5_Full_Project_Setup-HTML-SASS-Vanilla_JS-Bootstrap5/src/index.js
@@ -1,3 +1,11 @@
 import './scss/main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './js/app.js';
+import Tooltip from 'bootstrap/js/src/tooltip';
+
+/* Bootstrap bazı bileşenlerin javascript aktivitelerini kullanıcıya bırakmıştır. İlgili bileşenleri dahil sayfasından yapılması gerekenleri uygulayabilirsiniz.*/
+
+window.addEventListener("load", function() {
+    [...document.querySelectorAll('[data-bs-toggle="tooltip"]')]
+  .forEach(el => new Tooltip(el))
+})
